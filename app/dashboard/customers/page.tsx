6c1fa06785e431cc0556e54019395857028acdd5
Marta Solomacha <marciamarcia@o2.pl>
2026-05-24 21:51:50 +0200

    Add metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import Table from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+    </main>
+  );
 }
