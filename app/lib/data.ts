bf5d8f342df825d8571f3e95ef364de8b3f173fd
Marta Solomacha <marciamarcia@o2.pl>
2026-05-31 16:40:39 +0200

    Adjust tablet view and items per page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -76,10 +76,11 @@ export async function fetchCardData() {
   }
 }
 
-const ITEMS_PER_PAGE = 6;
+export const ITEMS_PER_PAGE = 10;
+
 export async function fetchFilteredInvoices(
   query: string,
-  currentPage: number
+  currentPage: number,
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
@@ -177,7 +178,7 @@ export async function fetchCustomers() {
 
 export async function fetchFilteredCustomers(
   query: string,
-  currentPage: number
+  currentPage: number,
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
